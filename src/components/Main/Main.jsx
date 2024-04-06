48ac26427fe4734906bbbdb35f7fddccdf5ec6fe
Vasily Demin <vasily.demin@gmail.com>
2024-04-06 20:56:40 +0300

    Data is loading message added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,34 @@
 import Column from "../Column/Column.jsx";
 import {statusList} from "../../data.js";
+import {useEffect, useState} from "react";
+
 
 function Main({cards}) {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000); // 2 секунды задержки
+
+    }, []);
+
+
     return (<main className="main">
         <div className="container">
             <div className="main__block">
                 <div className="main__content">
-                    { statusList.map((status) => (
-                            <Column
-                                key={status}
-                                columnTitle={status}
-                                cardList={cards.filter((card) => card.status === status)}
-                            />
-                    ))}
+                    {
+                        isLoading ? (<div>Данные загружаются</div>) :
+                            ( statusList.map((status) => (
+                                <Column
+                                    key={status}
+                                    columnTitle={status}
+                                    cardList={cards.filter((card) => card.status === status)}
+                                />
+                            )))
+                    }
                 </div>
 
             </div>
