99c4d1fe8723f7182abec54801f047bace234e51
Vasily Demin <vasily.demin@gmail.com>
2024-04-06 20:44:41 +0300

    PopUser window display on/off implemented, cards array, mapping and filtering added to Column and App components, new task adding by pressing 'create new task' button

--- src/components/PopUser/PopUser.jsx
+++ src/components/PopUser/PopUser.jsx
@@ -1,31 +1,28 @@
-import {useRef} from "react";
+import {useState} from "react";
 
 function PopUser() {
 
-    const userRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
 
     const onClick = () => {
-        if (userRef.current) {
-            if(userRef.current.display === 'none') {
-                userRef.current.display='block';
-            } else {
-                userRef.current.display='none';
-            }
-        }
+        setIsVisible((prevState) => !prevState);
     }
 
     return(
         <div>
             <a href="#user-set-target" className="header__user _hover02" onClick={onClick}>Ivan Ivanov</a>
-            <div className="header__pop-user-set pop-user-set" id="user-set-target" ref={userRef}>
-                <p className="pop-user-set__name">Ivan Ivanov</p>
-                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                <div className="pop-user-set__theme">
-                    <p>Темная тема</p>
-                    <input type="checkbox" className="checkbox" name="checkbox"/>
-                </div>
-                <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
-            </div>
+            { isVisible && (
+                    <div className="header__pop-user-set pop-user-set" id="user-set-target">
+                        <p className="pop-user-set__name">Ivan Ivanov</p>
+                        <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                        <div className="pop-user-set__theme">
+                            <p>Темная тема</p>
+                            <input type="checkbox" className="checkbox" name="checkbox"/>
+                        </div>
+                        <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
+                    </div>
+                )
+            }
         </div>
     );
 }
