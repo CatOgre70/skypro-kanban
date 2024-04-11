9010f1349884f7e468d46706d67b165bf0cf81c5
Vasily Demin <vasily.demin@gmail.com>
2024-04-11 22:47:02 +0300

    Destroyed PopUser window design :(

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,23 +1,31 @@
+import {
+    ContainerSignIn,
+    Modal,
+    ModalBlock,
+    ModalBtnEnter, ModalFormGroup,
+    ModalFormLogin,
+    ModalInput,
+    ModalTitleH2
+} from "./Login.styled.js";
+
 function Login() {
     return(
-            <div className="container-signin">
-                <div className="modal">
-                    <div className="modal__block">
-                        <div className="modal__ttl">
-                            <h2>Вход</h2>
-                        </div>
-                        <form className="modal__form-login" id="formLogIn" action="#">
-                            <input className="modal__input" type="text" name="login" id="formlogin" placeholder="Эл. почта"/>
-                            <input className="modal__input" type="password" name="password" id="formpassword" placeholder="Пароль"/>
-                            <button className="modal__btn-enter _hover01" id="btnEnter"><a href="../main.html">Войти</a></button>
-                            <div className="modal__form-group">
+            <ContainerSignIn>
+                <Modal>
+                    <ModalBlock>
+                        <ModalTitleH2>Вход</ModalTitleH2>
+                        <ModalFormLogin id="formLogIn" action="#">
+                            <ModalInput type="text" name="login" id="formlogin" placeholder="Эл. почта"/>
+                            <ModalInput type="password" name="password" id="formpassword" placeholder="Пароль"/>
+                            <ModalBtnEnter id="btnEnter"><a href="../main.html">Войти</a></ModalBtnEnter>
+                            <ModalFormGroup>
                                 <p>Нужно зарегистрироваться?</p>
                                 <a href="signup.html">Регистрируйтесь здесь</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                            </ModalFormGroup>
+                        </ModalFormLogin>
+                    </ModalBlock>
+                </Modal>
+            </ContainerSignIn>
     )
 }
 
